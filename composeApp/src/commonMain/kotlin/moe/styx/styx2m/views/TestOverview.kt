b2defec93a39c08a47225d258eaac4a0f031a912
Alex H <48185123+Vodes@users.noreply.github.com>
2024-02-28 18:58:50 +0100

    Implement MediaSearch

--- composeApp/src/commonMain/kotlin/moe/styx/styx2m/views/TestOverview.kt
+++ composeApp/src/commonMain/kotlin/moe/styx/styx2m/views/TestOverview.kt
@@ -1,44 +1,67 @@
 package moe.styx.styx2m.views
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.core.screen.Screen
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.runBlocking
 import moe.styx.common.compose.components.anime.AnimeCard
 import moe.styx.common.compose.components.layout.MainScaffold
+import moe.styx.common.compose.components.search.MediaSearch
 import moe.styx.common.compose.files.Storage
 import moe.styx.common.compose.files.getCurrentAndCollectFlow
 import moe.styx.common.compose.utils.LocalGlobalNavigator
+import moe.styx.common.compose.utils.SearchState
+import moe.styx.common.extension.eqI
+import moe.styx.common.extension.toBoolean
 import moe.styx.styx2m.views.anime.AnimeDetailView
 
 class TestOverview : Screen {
 
-    @OptIn(ExperimentalFoundationApi::class)
+    @OptIn(ExperimentalFoundationApi::class, FlowPreview::class)
     @Composable
     override fun Content() {
         val nav = LocalGlobalNavigator.current
         val media by Storage.stores.mediaStore.getCurrentAndCollectFlow()
+        val categories by Storage.stores.categoryStore.getCurrentAndCollectFlow()
+        val searchStore = Storage.stores.showSearchState
+        val filtered = media.filter { it.isSeries.toBoolean() }
+        val availableGenres = filtered.flatMap { m ->
+            (m.genres ?: "").split(",").map { it.trim() }
+        }.distinct().filter { it.isNotBlank() }.sorted().toList()
+        val availableCategories =
+            filtered.asSequence().map { (it.categoryID ?: "").trim() }
+                .distinct().filter { it.isNotBlank() }
+                .mapNotNull { categories.find { cat -> cat.GUID eqI it } }
+                .filter { it.isVisible.toBoolean() }.sortedBy { it.sort }.toList()
+        val initialState = runBlocking { searchStore.get() ?: SearchState() }
+        val mediaSearch = MediaSearch(searchStore, initialState, availableGenres, availableCategories)
         MainScaffold(title = "Test", addPopButton = false) {
             Column(Modifier.fillMaxSize()) {
-                LazyVerticalGrid(
-                    columns = GridCells.Fixed(3),
-                    contentPadding = PaddingValues(10.dp, 7.dp),
-                ) {
-                    itemsIndexed(
-                        items = media, key = { _, item -> item.GUID },
-                    ) { _, item ->
-                        Row(modifier = Modifier.animateItemPlacement()) {
-                            AnimeCard(item, false) {
-                                nav.push(AnimeDetailView(item.GUID))
+                mediaSearch.Component(Modifier.fillMaxWidth().padding(10.dp))
+                Column(Modifier.fillMaxSize()) {
+                    val flow by mediaSearch.stateEmitter.debounce(150L).collectAsState(initialState)
+                    val processedMedia = flow.filterMedia(media)
+                    LazyVerticalGrid(
+                        columns = GridCells.Fixed(3),
+                        contentPadding = PaddingValues(10.dp, 7.dp),
+                    ) {
+                        itemsIndexed(
+                            items = processedMedia, key = { _, item -> item.GUID },
+                        ) { _, item ->
+                            Row(modifier = Modifier.animateItemPlacement()) {
+                                AnimeCard(item, false) {
+                                    nav.push(AnimeDetailView(item.GUID))
+                                }
                             }
                         }
                     }
--- composeApp/src/commonMain/kotlin/moe/styx/styx2m/views/anime/AnimeDetailView.kt
+++ composeApp/src/commonMain/kotlin/moe/styx/styx2m/views/anime/AnimeDetailView.kt
@@ -35,10 +35,7 @@ import moe.styx.common.compose.components.anime.MediaNameListing
 import moe.styx.common.compose.components.anime.MediaRelations
 import moe.styx.common.compose.components.buttons.FavouriteIconButton
 import moe.styx.common.compose.components.layout.MainScaffold
-import moe.styx.common.compose.extensions.getPath
-import moe.styx.common.compose.extensions.getThumb
-import moe.styx.common.compose.extensions.getURL
-import moe.styx.common.compose.extensions.isCached
+import moe.styx.common.compose.extensions.*
 import moe.styx.common.compose.files.Storage
 import moe.styx.common.compose.files.getCurrentAndCollectFlow
 import moe.styx.common.compose.settings
@@ -73,9 +70,10 @@ class AnimeDetailView(private val mediaID: String) : Screen {
                 ElevatedCard(Modifier.padding(2.dp).fillMaxSize()) {
                     if (this@BoxWithConstraints.maxWidth < 550.dp) {
                         Column {
-                            MetadataArea(media, nav, mediaList)
-                            Divider(Modifier.fillMaxWidth().padding(10.dp, 8.dp), thickness = 3.dp)
-                            EpisodeList(entries, showSelection, null) { "" }
+                            EpisodeList(entries, showSelection, null, { "" }) {
+                                MetadataArea(media, nav, mediaList)
+                                Divider(Modifier.fillMaxWidth().padding(10.dp, 8.dp), thickness = 3.dp)
+                            }
                         }
                     } else {
                         val scrollState = rememberScrollState()
@@ -85,7 +83,7 @@ class AnimeDetailView(private val mediaID: String) : Screen {
                             }
                             Divider(Modifier.padding(2.dp, 8.dp).fillMaxHeight().width(3.dp))
                             Column(Modifier.weight(0.5F)) {
-                                EpisodeList(entries, showSelection, null) { "" }
+                                EpisodeList(entries, showSelection, null, { "" })
                             }
                         }
                     }
@@ -98,7 +96,9 @@ class AnimeDetailView(private val mediaID: String) : Screen {
     fun fetchEntries(): List<MediaEntry> {
         val flow by Storage.stores.entryStore.getCurrentAndCollectFlow()
         val filtered = flow.filter { it.mediaID eqI mediaID }
-        return filtered
+        return if (settings["episode-asc", false]) filtered.sortedBy {
+            it.entryNumber.toDoubleOrNull() ?: 0.0
+        } else filtered.sortedByDescending { it.entryNumber.toDoubleOrNull() ?: 0.0 }
     }
 }
 
@@ -107,7 +107,7 @@ fun MetadataArea(media: Media, nav: Navigator, mediaList: List<Media>, nameImage
     StupidImageNameArea(media, nameImageModifier)
     AboutView(media)
     if (!media.sequel.isNullOrBlank() || !media.prequel.isNullOrBlank()) {
-        Divider(Modifier.fillMaxWidth().padding(10.dp, 4.dp, 10.dp, 2.dp), thickness = 2.dp)
+        Divider(Modifier.fillMaxWidth().padding(10.dp, 4.dp, 10.dp, 5.dp), thickness = 2.dp)
         MediaRelations(media, mediaList) { nav.replace(AnimeDetailView(it.GUID)) }
     }
 }
@@ -120,7 +120,7 @@ fun AboutView(media: Media) {
     MediaGenreListing(media)
     if (!synopsis.isNullOrBlank()) {
         SelectionContainer {
-            Text(synopsis, Modifier.padding(6.dp), style = MaterialTheme.typography.bodyMedium)
+            Text(synopsis.removeSomeHTMLTags(), Modifier.padding(6.dp), style = MaterialTheme.typography.bodyMedium)
         }
     }
 }
